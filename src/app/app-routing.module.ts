cda605a7ceb05d9a0fb687aaa572dcfa25292a65
Bapa <bapa.bc04@gmail.com>
2024-08-28 17:33:07 -0400

    Created components, pages, home, posts, list-posts, new-posts and post folders with all their components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'home', 
+    loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) 
+  },
+  { 
+    path: 'posts', 
+    loadChildren: () => import('./components/posts/list-posts/list-posts.module').then(m => m.ListPostsModule) 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
