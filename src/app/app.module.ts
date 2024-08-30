b6a724ba24eb072d5b4d354b32a5b650bd768b12
Bapa <bapa.bc04@gmail.com>
2024-08-30 16:04:46 -0400

    Created a toolbar and about folders and their components. Added the code and styles to about.component. Added an active property in styles.css so when the 'a' element from toolbar.component is active it will have a border. Added the 'about' path to app-routing.module.ts. Added the <app-toolbar> element to app.component.html. Imported the 'MaterialModule' to about.module.ts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { NewPostComponent } from './components/posts/new-post/new-post.component';
-import { NewPostModule } from './components/posts/new-post/new-post.module';
 import { PostComponent } from './components/posts/post/post.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ToolbarComponent } from './shared/components/toolbar/toolbar.component';
+
 import { MaterialModule } from './material.module';
+import { NewPostModule } from './components/posts/new-post/new-post.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewPostComponent,
-    PostComponent
+    PostComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NewPostModule,
+    BrowserAnimationsModule,
     MaterialModule
   ],
   providers: [
