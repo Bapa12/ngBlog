b6dc807acc2fa7deaad2d95a50c4f45de0cbab46
Bapa <bapa.bc04@gmail.com>
2024-08-30 13:10:20 -0400

    Added code to 'post.component.html' to show an individual post in another link. Changed the image of 'Post One' in home.component.ts. Created a 'public post' variable in post.component.ts and imported 'ActivatedRoute' to go to the post individual page according to an id and added styles in home and post.component.scss

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -16,7 +16,7 @@ export class HomeComponent {
       id: "1",
       titlePost: "Post One",
       contentPost: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-      imagePost: "https://vegans.uk/wp-content/uploads/2014/11/Chewie-on-chair-200x300.jpg"
+      imagePost: "https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg"
     },
     {
       id: "2",
--- src/app/components/posts/post/post.component.ts
+++ src/app/components/posts/post/post.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-post',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './post.component.scss'
 })
 export class PostComponent {
+  public post: {
+    id: string,
+    titlePost: string,
+    contentPost: string,
+    imagePost: string
+  } = {
+    id: "1",
+    titlePost: "Post One",
+    contentPost: "Hola Mundo",
+    imagePost: "https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg"
+  }
+
+  constructor (private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.post.id = this.route.snapshot.params['id'];
+  }
 
 }
